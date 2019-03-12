ff2479c30091e287f4e2f66f428c40ced843aecb
Julaika <47170634+Julaika@users.noreply.github.com>
2019-03-12 10:37:49 +0530

    Update and rename beg16.py to beg9.py

--- beg9.py
+++ beg9.py
@@ -1,10 +1,13 @@
 x,y=input().split()
+c=0
 x=int(x)
 y=int(y)
-for i in range(x,y):
+for i in range(x,y+1):
   for num in range(2,i):
     if(i % num) == 0:
-       continue
-    else:
-      print(num)
+       break
+  else:
+    c+=1
+print(c) 
+
  
